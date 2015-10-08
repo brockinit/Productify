d0c0307cb44983f7fb902db090240c013d1e8fb2
Brock Lanoza <brocklanoza@gmail.com>
2015-10-07 15:36:50 -1000

    cleaned out structure files.

---
+++ server/security.js
@@ -0,0 +1,12 @@
+// Posts.allow({
+//   'insert' : function (userId,doc) {
+//     /* user and doc checks ,
+//     return true to allow insert */
+//     return (userId === Meteor.userId);
+//   },
+//   'update' : function (userId,doc) {
+//     /* user and doc checks ,
+//     return true to allow update */
+//     return (userId === Meteor.userId);
+//   }
+// });
