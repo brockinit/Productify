595e4db087434b255ddb501bdc29bc3347d7424f
Alexander Anich <alexander.anich@gmail.com>
2015-10-09 10:26:51 -1000

    account templates routes configuration

--- client/views/intro.js
+++ client/views/intro.js
@@ -9,17 +9,3 @@ Template.introLayout.created = function() {
 Template.introLayout.destroyed = function() {
 
 }
-
-
-
-// Accounts.ui.config({
-//   passwordSignupFields : 'USERNAME_ONLY'
-// });
-
-
-Template.introLayout.events({
-  'click #at-btn' : function(event, template) {
-    event.preventDefault();
-    Router.go('/dashboard/:username');
-  }
-});
